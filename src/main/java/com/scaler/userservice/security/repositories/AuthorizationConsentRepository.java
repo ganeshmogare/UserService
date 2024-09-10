3e14bff27f0552927d22d2c84c9edfed59a87431
ganesh <mogriganpati18@gmail.com>
2024-09-10 17:12:47 +0530

    Authentication implementation

---
+++ src/main/java/com/scaler/userservice/security/repositories/AuthorizationConsentRepository.java
@@ -0,0 +1,14 @@
+package com.scaler.userservice.security.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.scaler.userservice.security.models.AuthorizationConsent;
+
+@Repository
+public interface AuthorizationConsentRepository extends JpaRepository<AuthorizationConsent, AuthorizationConsent.AuthorizationConsentId> {
+    Optional<AuthorizationConsent> findByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+    void deleteByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+}
---
+++ src/main/java/com/scaler/userservice/security/repositories/ClientRepository.java
@@ -0,0 +1,13 @@
+package com.scaler.userservice.security.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.scaler.userservice.security.models.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByClientId(String clientId);
+}
